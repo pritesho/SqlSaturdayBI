41a66ead12f1c748b094297e3ee3091d11907cb8
pritesho <pritesh_ostwal@yahoo.co.in>
2016-12-03 21:31:37 -0500

    Base version with working controller

---
+++ SqlSaturdayCoreApp/src/SqlSaturdayCoreUI/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SqlSaturdayCoreUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Reporting Portal"; ViewData["Message"] = "Reporting Portal"; ViewData["Message"] = "Reporting Portal"; ViewData["Message"] = "Reporting Portal"; ViewData["Message"] = "Reporting Portal"; ViewData["Message"] = "Reporting Portal"; ViewData["Message"] = "Reporting Portal"; ViewData["Message"] = "Reporting Portal";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Sql Saturday BI GA.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ SqlSaturdayCoreApp/src/SqlSaturdayCoreUI/Controllers/ReportController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SqlSaturdayCoreUI.Models.ReportViewModels;
+
+namespace SqlSaturdayCoreUI.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new ReportViewModel() { BrowserRemembered  = true });
+        }        
+    }
+}
